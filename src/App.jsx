ce05162fa8880f91b3e3c5fac30dc3eb8919fa62
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-06-07 17:37:10 +0200

    [feat] add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import TaskList from './pages/TaskList';
 import AddTask from './pages/AddTask';
+import { GlobalProvider } from './context/GlobalContex';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
 
-      <nav className='bg-primary py-3'>
-        <NavLink to='/' className='btn text-dark mx-4' style={{ backgroundColor: 'white' }}>Lista Task</NavLink>
-        <NavLink to='/add' className='btn text-dark' style={{ backgroundColor: 'white' }}>Aggiungi Task</NavLink>
-      </nav>
+        <nav className='bg-primary py-3'>
+          <NavLink to='/' className='btn text-dark mx-4' style={{ backgroundColor: 'white' }}>Lista Task</NavLink>
+          <NavLink to='/add' className='btn text-dark' style={{ backgroundColor: 'white' }}>Aggiungi Task</NavLink>
+        </nav>
 
-      <Routes>
-        <Route path='/' element={<TaskList />} />
-        <Route path='/add' element={<AddTask />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path='/' element={<TaskList />} />
+          <Route path='/add' element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContex'
 
 const TaskList = () => {
+
+  const { tasks } = useContext(GlobalContext)
+
   return (
-    <h2 className='my-4 mx-4 text-primary'>Lista delle Task</h2>
+    <div>
+      <h2 className='my-4 mx-4 text-primary'>Lista delle Task</h2>
+      <p className='mx-4'>Mostreremo i Task...</p>
+    </div>
   )
 }
 
